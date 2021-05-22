e8dc3ce0d2d42d4487a1d7ed48249a5338ea5cff
Nick Briggs <nicholas.h.briggs@gmail.com>
2021-05-22 16:40:37 -0700

    Fix RR leaks from cache_process() (#39)
    * Fix RR leaks from cache_process()
    
    Free RR entries that are leaked by cache_process when they are not incorporated
    into the cache.
    
    Closes haesbaert/mdnsd#37
    
    * Update mdns.c
    
    Missed a spot.
    
    Co-authored-by: Christiano Haesbaert <christiano.fh@gmail.com>

--- mdnsd/mdns.c
+++ mdnsd/mdns.c
@@ -155,8 +155,10 @@ cache_process(struct rr *rr)
 					/* TODO Cancel possible deletion */
 					log_info("cache_process: recover %s",
 					    rrs_str(&rr->rrs));
+					free(rr);
 					return (0);
 				}
+				free(rr);
 				return (0);
 			}
 			/*
@@ -167,6 +169,7 @@ cache_process(struct rr *rr)
 				log_warnx("cache_process: conflict for %s",
 				    rrs_str(&rr->rrs));
 				conflict_resolve_by_rr(rr_aux);
+				free(rr);
 				return (-1);
 			}
 		}
@@ -178,6 +181,7 @@ cache_process(struct rr *rr)
 					log_info("cache_process: goodbye %s",
 					    rrs_str(&rr->rrs));
 					cache_delete(rr_aux);
+					free(rr);
 					return (0);
 				}
 				/* Cache refresh */
@@ -186,14 +190,16 @@ cache_process(struct rr *rr)
 				rr_aux->ttl = rr->ttl;
 				rr_aux->revision = 0;
 				cache_schedrev(rr_aux);
-
+				free(rr);
 				return (0);
 			}
 		}
 	}
 	/* Got a goodbye for a record we don't have */
-	if (rr->ttl == 0)
+	if (rr->ttl == 0) {
+		free(rr);
 		return (0);
+	}
 
 	return (cache_insert(rr));
 }
